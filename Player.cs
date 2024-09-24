ba7582483cd6d0c11c38807d9e9945c1569d1d8f
o.o <96408296+00ovo00@users.noreply.github.com>
2024-09-24 19:54:07 +0900

    [feat] 휴식하기 기능 추가

--- Player.cs
+++ Player.cs
@@ -106,6 +106,49 @@ namespace TextRPG
                     return GameMode.ShowState;
             }
         }
+        // 플레이어가 휴식할 수 있도록 관리하는 함수
+        public GameMode ProcessGoShelter(Player player, ref MessageType msgtype)
+        {
+            int restPrice = 500;
+            Console.Clear();
+            Console.WriteLine("휴식하기");
+            Console.WriteLine($"{restPrice} G 를 내면 체력을 회복할 수 있습니다. (보유 골드 : {player.Gold} G)");
+            Console.WriteLine("\n1. 휴식하기");
+            Console.WriteLine("0. 나가기\n");
+
+            // 사용자 입력 처리
+            int input = utility.InputFromUser(ref msgtype);
+            switch (input)
+            {
+                case 0:
+                    msgtype = MessageType.Normal;
+                    return GameMode.Lobby;
+                case 1:
+                    msgtype = ProcessRest(player, restPrice);
+                    return GameMode.GoShelter;
+                default:
+                    msgtype = MessageType.WrongInput;
+                    return GameMode.GoShelter;
+            }
+        }
+        // 휴식 요청의 유효성 체크 및 휴식 실행하는 함수
+        MessageType ProcessRest(Player player, int price)
+        {
+            // 휴식 유효성 체크
+            if (player.Hp >= 100)   // 휴식 가능한 상태인지 확인
+                return MessageType.RestFailed;
+            if (player.Gold < price)    // 골드가 충분한지 확인
+                return MessageType.LackGold;
+            // 골드 차감하고 체력 회복, 최대 회복체력 100으로 제한
+            else
+            {
+                player.Gold -= price;
+                player.Hp += 100;
+                if (player.Hp >= 100)
+                    player.Hp = 100;
+                return MessageType.RestSucceed; // 휴식 완료
+            }
+        }
     }
     // 전사 클래스 (플레이어 클래스를 상속)
     class Warrior : Player
